9fb430e40c8e33d6a6913d75367121cf7f24389c
Hugo Placencia <hugojosua1997@hotmail.com>
2026-08-11 18:09:46 -0500

    Eliminar Notificaciones2

--- src/rutas/notificacionesRoutes.js
+++ src/rutas/notificacionesRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerPendientesNotificacion, enviarNotificacionIndividual } = require('../controlador/notificacionesController');
+const { obtenerPendientesNotificacion, enviarNotificacionIndividual, descartarNotificacion } = require('../controlador/notificacionesController');
 
 router.get('/pendientes', obtenerPendientesNotificacion);
 router.post('/enviar', enviarNotificacionIndividual);
+router.post('/descartar', descartarNotificacion);
 
 module.exports = router;
